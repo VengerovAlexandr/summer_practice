8498d9e1c0dc4ad0816fdc26ae2354dc449a9659
VengerovAlexandr <azot7002@gmail.com>
2026-07-10 23:17:47 +0300

    contest_2
    contest_2

---
+++ contest_2/contest_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    long long omega;
+    cin >> n >> omega;
+
+    vector<pair<long long, int>> holes(n);
+    for (int i = 0; i < n; i++) {
+        cin >> holes[i].first;
+        holes[i].second = i + 1;
+    }
+
+    sort(holes.begin(), holes.end());
+
+    vector<int> ans;
+    long long current = omega;
+    int i = 0;
+
+    while (i < n) {
+        if (holes[i].first < current) {
+            current += holes[i].first;
+            ans.push_back(holes[i].second);
+            i++;
+        }
+        else {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << endl;
+    for (int idx : ans) {
+        cout << idx << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
